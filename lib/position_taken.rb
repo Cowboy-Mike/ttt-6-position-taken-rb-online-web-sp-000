2673d5fdf68fc6ff244ac3556bfc109a6095dc32
Mike Nanto <mike@thecowboymechanic.com>
2020-12-18 08:25:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != " "
-  end
+    if board[index] == "2"
+        puts "right"
+    end
 end
+
+board = (" ", "2", " ")
+index = 1
+
+position_taken(board, 2)
